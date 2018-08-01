d2ffa34fe65b4fc48edb4a62ed122e674546d500
User <User@LAPTOP-JNLPDVKQ>
2018-07-31 19:46:39 -0700

    Initial Commit

--- src/jobsheet7/CobaPercabangan.java
+++ src/jobsheet7/CobaPercabangan.java
@@ -4,11 +4,36 @@
  * and open the template in the editor.
  */
 package jobsheet7;
-
+import java.util.Scanner;
 /**
  *
  * @author User
  */
 public class CobaPercabangan {
+    public static void main(String[] args) {
+        
+    String identitas = "Wahyu Aditya Utomo / XRPL5 / 41";
+    System.out.println("Identitas : "+ identitas);
+    Scanner scanner = new Scanner(System.in);
     
+   System.out.print("Nilai a ? ");
+   int a = scanner.nextInt();
+   
+        System.out.println("\nIF.. 1");
+        if(a < 5) System.out.println("nilai a kurang dari 5");
+        
+        System.out.println("\nIF.. 2");
+        if(a == 5)
+            System.out.println("nilai a sama dengan 5");
+        
+        System.out.println("\nIF.. 3");
+        if(a > 5) {
+            System.out.println("isi variabel a : "+ a);
+            System.out.println(" nilai a lebih dari 5");
+            
+        }
+    }
 }
+    
+    
+    
