cae1a21215694153e391270587618394be325c3d
User <User@LAPTOP-JNLPDVKQ>
2018-08-02 20:56:24 -0700

    Initial Commit

--- src/jobsheet7/CobaPercabangan.java
+++ src/jobsheet7/CobaPercabangan.java
@@ -4,95 +4,48 @@
  * and open the template in the editor.
  */
 package jobsheet7;
+
 import java.util.Scanner;
+
 /**
  *
- * @author User
+ * @author ASUS
  */
 public class CobaPercabangan {
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
+        String identitas = "Krisna Putra Mariyanto / X RPL 5 / 18";
+        System.out.println("Identitas : " + identitas);
         
-    String identitas = "Wahyu Aditya Utomo / XRPL5 / 41";
-    System.out.println("Identitas : "+ identitas);
-    Scanner scanner = new Scanner(System.in);
-    
-   System.out.print("Nilai a ? ");
-   int a = scanner.nextInt();
-   
+        Scanner scanner = new Scanner(System.in);
+        // TODO code application logic here
+        System.out.print("Nila a ? ");
+        int a = scanner.nextInt();
+        
         System.out.println("\nIF.. 1");
-        if(a < 5) System.out.println("nilai a kurang dari 5");
+        if (a <5)System.out.println("nilai a kurang dari 5");
         
-        System.out.println("\nIF.. 2");
-        if(a == 5)
-            System.out.println("nilai a sama dengan 5");
+        System.out.println("\nIf.. 2");
+        if (a==5) System.out.println("nilai a sama dengan 5");
         
-        System.out.println("\nIF.. 3");
-        if(a > 5) {
-            System.out.println("isi variabel a : "+ a);
-            System.out.println(" nilai a lebih dari 5");
-           
-            System.out.println("\nIF..ELSE..");
-            if(a < 5) {
-                System.out.println("isi variabel a : "+ a);
-                System.out.println("nilai a kurang dari 5");  
-            } else System.out.println("nilai a lebih dari atau sama dengan 5");
-            System.out.println("\nIF..ELSE IF..ELSE..1");
-            if(a < 5) {
-                System.out.println("nilai a kurang dari 5");
-            } else if(a == 5) {
-                System.out.println("nilai a sama dengan 5");
-            } else
-                System.out.println("nilai a lebih dari 5");
-            
-            System.out.println("\nIF..ELSE IF..ELSE.. 2");
-            if(a < 2) {
-                System.out.println("nilai a kurang dari 2");
-            }else if(a < 4) {
-                System.out.println("nilai a kurang dari 4");
-            }else if(a < 6) {
-                System.out.println("nilai a kurang dari 6");
-            }else if(a == 6) {
-                System.out.println("nilai a sama dengan 6");
-            }else {
-                System.out.println("nilai a lebih dari 6");
-            }
-            
-            System.out.println("\nNested IF");
-            if(a < 7) {
-                System.out.println("nilai a kurang dari 7");
-                if(a > 2)
-                    System.out.println("nilai a lebih dari 2");
-                if(a < 4)
-                    System.out.println("nilai a kurang dari 4");
-            }
-            
-            System.out.println("\nSWITCH..CASE");
-            switch(a) {
-                case 1: System.out.println("nilai a => 1"); break;
-                case 2: System.out.println("nilai a => 2"); break;
-                case 3:
-                case 4: System.out.println("nilai a => 3 atau 4"); break;
-                case 5: System.out.println("nilai a => 5"); break;
-                default: System.out.println("nilai a bukan antara 1-5");
-            }
-            
-            
-               
-            
-                
-            
-                     
+        System.out.println("\nIf.. 3");
+        if (a>5) 
+            System.out.println("nilai a lebih dari 5");
         
-
-                
+        System.out.println("\nIF..ELSE..");
+        if (a<5) {
+            System.out.println("isi variabel a : " +a);
+            System.out.println("nilai a kurang dari 5");
+       }else {
+               System.out.println("nilai variabel a lebih dari atau sama dengan 5");
+        }
+        
+        
                     
-                        
-                
-            
-            
-        
-    
-
-    
+        }
+    }
     
     
