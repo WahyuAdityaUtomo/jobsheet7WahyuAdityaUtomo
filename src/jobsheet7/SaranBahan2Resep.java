c7882665770253c715572b0384159389aa2412bc
User <User@LAPTOP-JNLPDVKQ>
2018-08-02 21:30:28 -0700

    Initial Commit

---
+++ src/jobsheet7/SaranBahan2Resep.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet7;
+
+/**
+ *
+ * @author User
+ */
+public class SaranBahan2Resep {
+    public static void main(String[] args) {
+        String identitas = "nama lengkap / kelas / no";
+        System.out.println("Wahyu Aditya Utomo : "+ identitas);
+        
+        System.out.println("\nSaran Resep dari Bahan Milik Anda\n");
+        
+    }
+    
+    
+}
